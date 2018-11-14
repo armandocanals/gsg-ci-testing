57819f41ce62540a37c4bcdac926a7e1499ab349
Armando Canals <armandocanals@gmail.com>
2018-11-14 00:49:52 -0800

    Add tests and examples

---
+++ math.test.js
@@ -0,0 +1,28 @@
+const add = require('./math').add;
+const subtract = require('./math').subtract;
+const divide = require('./math').divide;
+const multiply = require('./math').multiply;
+
+describe('add', () => {
+  it('should add our numbers', () => {
+    expect(add(2,2)).toBe(4);
+  });
+});
+
+describe('subtract', () => {
+  it('should subtract our numbers', () => {
+    expect(subtract(2,1)).toBe(1);
+  });
+});
+
+describe('divide', () => {
+  it('should divide our numbers', () => {
+    expect(divide(10,2)).toBe(5);
+  });
+});
+
+describe('multiply', () => {
+  it('should multiply our numbers', () => {
+    expect(multiply(5,5)).toBe(25);
+  });
+});
